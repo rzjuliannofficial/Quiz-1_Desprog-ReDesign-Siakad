67a5bbca41ce37415e5ea3445e2ccdc763fca0e8
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-10-14 19:40:15 +0700

    add logik for login and nav

--- script/main.js
+++ script/main.js
@@ -1,10 +1,49 @@
+const VALID_NIM = '2341720255';
+const VALID_PASSWORD = '123';
+
 // JavaScript untuk mengelola interaksi navbar 
 document.addEventListener('DOMContentLoaded', () => {
+
+    const loginForm = document.getElementById('loginForm');
+    
+    if (loginForm) {
+        // Dengarkan event 'submit' pada form
+        loginForm.addEventListener('submit', function(e) {
+            e.preventDefault(); // Mencegah pengiriman form standar
+            
+            // Ambil nilai dari input form
+            // ambil elemen input by id
+            const nimInput = document.getElementById('nim');
+            const passwordInput = document.getElementById('password');
+
+            //ambil isinya
+            const nim = nimInput.value;
+            const password = passwordInput.value;
+            
+            // Logika Validasi login
+            if (nim === VALID_NIM && password === VALID_PASSWORD) {
+                
+                window.location.href = 'Beranda.php';
+            } else {
+                // Jika validasi GAGAL, tampilkan alert error
+                const errorMessage = 'NIM atau Password salah. Silahkan coba lagi.';
+                alert(errorMessage);
+                
+                //hapus inputan 
+                passwordInput.value = '';
+            }
+        });
+    }
+
     const arrow = document.querySelector('.arrow');
     const nav = document.querySelector('.right-side nav');
-
     // Toggle class 'active' untuk menampilkan/menyembunyikan navbar
-    arrow.addEventListener('click', () => {
-        nav.classList.toggle('active');
-    });
+    if (arrow && nav) { //jadi menggunakan if karena event 'click' umum dipake. agar mengurangi error
+        arrow.addEventListener('click', () => {
+            nav.classList.toggle('active');
+        });
+    }
 });
+
+
+
