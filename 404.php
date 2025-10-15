628a8f478829ed6cbe14d4b503b7487c6046e628
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-10-15 08:41:34 +0700

    add 404 page

---
+++ 404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman tidak ditemukan</title>
+    <link rel="stylesheet" href="style/style404.css">
+</head>
+<body>
+    <img src="img/404.png" alt="">
+    <h1>Halaman tidak ditemukan</h1>
+    <p>Kami tidak dapat menemukan halaman yang Anda cari</p>
+    <a href="Beranda.php">
+        <button>
+            Kembali Ke Halaman Awal
+        </button>
+    </a>
+</body>
+</html>
